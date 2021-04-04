b6b42b522de09227edc8d8b6f34a1a944d194fa3
Syed Tirmizi <syedmtirmizi@gmail.com>
2021-04-04 12:55:44 -0400

    get weather icons to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,43 +24,47 @@ function locationSearch() {
         })
         .then(function(weatherData) {
             console.log(weatherData);
-            console.log(weatherData.current.temp)
+            // console.log(weatherData.current.temp)
             // var locationIconEl = document.querySelector('location-icon');
             // var getlocationIcon = document.createElement('svg');
-            var temperatureEl = document.querySelector('#temperature');
-            var humidityEl = document.querySelector('#humidity');
+
             var windSpeedEl = document.querySelector('#wind-speed');
             var uvEl = document.querySelector('#uv');
-            // locationIconEl.innerHTML = "";
-            // getlocationIcon.setAttribute('src', weatherData.current.weather[0].icon);
-            // locationIconEl.appendChild(getlocationIcon);
-            temperatureEl.innerHTML = weatherData.current.temp + "°F";
-            humidityEl.innerHTML = weatherData.current.humidity +"%";
+
+
             windSpeedEl.innerHTML = weatherData.current.wind_speed + "MPH";
             uvEl.innerHTML = " " + weatherData.current.uvi + " ";
 
-            var date1El = document.querySelector('#date-1');
-            var weatherIcon1 = document.querySelector('#weather-icon-1');
-            var temperature1El = document.querySelector('#temperature-1');
-            var humidity1El = document.querySelector('#humidity-1')
+            for (var i = 0; i < 6; i++) {
+                var unixTime = new Date(weatherData.daily[i].dt * 1000);
+                // console.log(unixTime);
+                var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+                var year = unixTime.getFullYear();
+                var month = months_arr[unixTime.getMonth()];
+                var day = unixTime.getDate();
+                var convdataTime = month+'-'+day+'-'+year;
+                // console.log(convdataTime);
 
-            var unixTime1 = Date(weatherData.daily[1].dt * 1000);
-            console.log(unixTime1);
-            var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-            var year = unixTime1.getFullYear();
-            var month = months_arr[unixTime1.getMonth()];
-            var day = unixTime1.getDate();
-            var convdataTime = month+'-'+day+'-'+year;
-            console.log(convdataTime);
+                var dateEl = document.querySelector('#date-' + i +'');
+                dateEl.innerHTML =  convdataTime;
+
+                var temperatureEl = document.querySelector('#temperature-' + i + '');
+                var humidityEl = document.querySelector('#humidity-' + i +'')
+                var weatherIconEl = document.querySelector('#weather-icon-' + i + '');
+
+                console.log(weatherIconEl);
+                console.log(weatherData.daily[i].weather[0].icon)
 
 
+                temperatureEl.innerHTML = weatherData.daily[i].temp.max + "°F";
+                humidityEl.innerHTML = weatherData.daily[i].humidity +"%";
+                var icon = weatherData.daily[i].weather[0].icon 
+                console.log(icon);
+
+                weatherIconEl.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`;
 
 
-
-
-            
-            temperature1El.innerHTML = weatherData.daily[1].temp.max + "°F";
-            humidity1El.innerHTML = weatherData.daily[1].humidity + "%";
+            };
 
             
             
